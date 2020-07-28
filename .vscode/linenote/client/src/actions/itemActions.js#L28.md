f2ae3057fb14ecb7cd847b2b265d7d8f7b2166d3
mohur <m.ashrahfi@gmail.com>
2020-07-28 15:17:55 -0700

    deleting db items from frontend with axios

---
+++ .vscode/linenote/client/src/actions/itemActions.js#L28.md
@@ -0,0 +1,1 @@
+id we want to delete
--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -16,7 +16,7 @@ class ShoppingList extends Component {
         this.props.getItems();
     }
 
-    onDeletClick = (id) => {
+    onDeleteClick = (id) => {
         this.props.deleteItem(id);
     }
 
@@ -27,14 +27,14 @@ class ShoppingList extends Component {
 
                 <ListGroup>
                     <TransitionGroup className="shopping-list">
-                        {items.map(({ id, name }) => (
-                            <CSSTransition key={id} timeout={500} classNames="fade">
+                        {items.map(({ _id, name }) => (
+                            <CSSTransition key={_id} timeout={500} classNames="fade">
                                 <ListGroupItem>
                                     <Button 
                                         className="remove-btn"
                                         color="danger"
                                         size="sm"
-                                        onClick={this.onDeletClick.bind(this, id)}
+                                        onClick={this.onDeleteClick.bind(this, _id)}
                                     >&times;</Button>
                                     {name}
                                 </ListGroupItem>
