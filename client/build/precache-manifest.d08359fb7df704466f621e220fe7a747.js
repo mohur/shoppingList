d4a021bf1437cb64466b1d84903de1846607e3e3
mohur <m.ashrahfi@gmail.com>
2020-09-18 17:58:07 -0700

    deploying test #1

---
+++ client/build/precache-manifest.d08359fb7df704466f621e220fe7a747.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04bc751a4b578bba1b4ec57c6f05c1e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5b1e6e3556e76a50743",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b8a02e9f08468aaebfe4",
+    "url": "/static/css/main.6f6909c7.chunk.css"
+  },
+  {
+    "revision": "a5b1e6e3556e76a50743",
+    "url": "/static/js/2.f090292a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f090292a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8a02e9f08468aaebfe4",
+    "url": "/static/js/main.90ddacb4.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
